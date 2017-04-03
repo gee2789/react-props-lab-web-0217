6d67edcf47e5de5013e6446fb8e1a6f047ff0cde
gee2789 <geeyung.li@gmail.com>
2017-04-02 21:47:39 -0400

    Done.

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const ReactDOM = require('react-dom');
 const Spaceship = require('./components/Spaceship');
 
 ReactDOM.render(
-  <Spaceship name="Millennium Falcon" />,
+  <Spaceship
+    name="Millennium Falcon"
+    speed="1.2 parsecs"
+    hasRockets=true
+    colors=['red', 'silver', 'carbonite']
+    />,
   document.getElementById('main')
 );
 
